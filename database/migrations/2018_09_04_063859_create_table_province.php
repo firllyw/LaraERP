d8397e18f3f60d75a64b90cabfcbbea8bb56c69b
ASTRAGRAPHIA\FirlyW <fwahyudi17@gmail.com>
2018-09-04 13:48:35 +0700

    migration table country,province and city

---
+++ database/migrations/2018_09_04_063859_create_table_province.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProvince extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->unsignedInteger('country_id');
+            $table->timestamps();
+
+            $table->foreign('country_id')->references('id')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provinces');
+    }
+}
---
+++ database/migrations/2018_09_04_063938_create_table_city.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCity extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->unsignedInteger('province_id');
+            $table->timestamps();
+
+            $table->foreign('province_id')->references('id')->on('provinces');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
